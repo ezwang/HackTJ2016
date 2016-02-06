7d36dad2e7913d21f4a1c96bf8af68fadf610120
Grey <bobobalink@gmail.com>
2016-02-06 18:56:30 -0500

    added material bootstrap stuff

--- server.py
+++ server.py
@@ -4,9 +4,11 @@ from flask import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True, debug=True)
