2309e2b46614da4280f5fd2c9b9d8da0df6437c3
Grey <bobobalink@gmail.com>
2016-02-06 18:08:07 -0500

    fixed server.py and added idea to the gitignore

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 from flask import *
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return '<marquee>WordPolo</marquee>'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', threaded=False)
