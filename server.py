c3cc9cfca300bbdcca8b883d143d8b68f08cb9fe
Eric Wang <ezwang1@gmail.com>
2016-02-06 17:58:35 -0500

    basic server on port 5000

--- server.py
+++ server.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
 from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
