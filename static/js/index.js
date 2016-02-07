faf40be96d0a3a7d978f6b85298bd40e1bd0cffa
Eric Wang <ezwang1@gmail.com>
2016-02-06 23:50:00 -0500

    added speech recognition

---
+++ static/js/index.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    navigator.getUserMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    window.URL = window.URL || window.webkitURL;
+
+    var audio_context = new AudioContext;
+
+    if (!('webkitSpeechRecognition' in window)) {
+        upgrade();
+    }
+
+    $("#add-word").click(function(e) {
+        e.preventDefault();
+        if (!$("#new-word").val()) {
+            return;
+        }
+        $("#btn-toggle").prop("disabled", false);
+        $("#word-list").append($("<div class='word-item'><span class='word-item-text'>" + $("<div />").text($("#new-word").val()).html() + "</span><span class='word-item-delete'>&times;</span></div>"));
+        $("#new-word").val("");
+    });
+    $("#new-word").keyup(function(e) {
+        if (e.keyCode == 13) {
+            $("#add-word").click();
+        }
+    });
+    $("#word-list").on("click", ".word-item-delete", function(e) {
+        e.preventDefault();
+        $(this).parent().remove();
+        if ($("#word-list .word-item").length == 0) {
+            $("#btn-toggle").prop("disabled", true);
+            playing = false;
+        }
+    });
+    var playing = false;
+    $("#btn-toggle").click(function(e) {
+        e.preventDefault();
+        if ($("#word-list .word-item").length == 0) {
+            return;
+        }
+        playing = !playing;
+        $("#btn-toggle").text(!playing ? 'Start Practice' : 'Stop Practice');
+        load_practice_word($("#word-list .word-item").eq(Math.floor(Math.random() * $("#word-list .word-item").length)).find(".word-item-text").text());
+    });
+});
+function quiz_word() {
+    var final_transcript = '';
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = 'zh-CN';
+    recognition.onresult = function(event) {
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                if (event.results[i][0].transcript) {
+                    final_transcript += event.results[i][0].transcript;
+                }
+            }
+        }
+        if (final_transcript) {
+            console.log(final_transcript);
+        }
+    }
+    recognition.start();
+}
+function load_practice_word(word) {
+    $("#practice-word").text(word);
+}
