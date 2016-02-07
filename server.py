6e43278f894a963160f82f827f65188d133ef446
czhao <czhao39@gmail.com>
2016-02-07 01:17:26 -0500

    Added python function that returns whether or not input is correct

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python2
 
 from flask import *
+import json
 
 app = Flask(__name__, static_url_path='')
-
+with open('Uni2Pinyin.json') as the_file:
+    pinyin = json.load(the_file)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/uni2pinyin')
+def getPinYin():
+    s = ord(request.args.get('spoken'))
+    a = ord(request.args.get('actual'))
+    return len(set(pinyin[s]) & set(pinyin[a])) > 0
 
 
 if __name__ == '__main__':
